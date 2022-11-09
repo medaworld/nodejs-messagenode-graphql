065d98071e354081cc64de71fa3db4e62a5654b5
MEDA <118144136+medaworld@users.noreply.github.com>
2022-11-09 15:03:49 -0800

    Add get posts and pagination

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -7,26 +7,26 @@ const Post = require('../models/post');
 
 module.exports = {
   createUser: async function ({ userInput }, req) {
-    // const email = userInput.email;
+    //   const email = args.userInput.email;
     const errors = [];
     if (!validator.isEmail(userInput.email)) {
-      errors.push({ message: 'Email is invalid' });
+      errors.push({ message: 'E-Mail is invalid.' });
     }
     if (
       validator.isEmpty(userInput.password) ||
       !validator.isLength(userInput.password, { min: 5 })
     ) {
-      errors.push({ message: 'Password too short' });
+      errors.push({ message: 'Password too short!' });
     }
     if (errors.length > 0) {
-      const error = new Error('Invalid input');
+      const error = new Error('Invalid input.');
       error.data = errors;
       error.code = 422;
       throw error;
     }
     const existingUser = await User.findOne({ email: userInput.email });
     if (existingUser) {
-      const error = new Error('User exists already');
+      const error = new Error('User exists already!');
       throw error;
     }
     const hashedPw = await bcrypt.hash(userInput.password, 12);
@@ -38,17 +38,16 @@ module.exports = {
     const createdUser = await user.save();
     return { ...createdUser._doc, _id: createdUser._id.toString() };
   },
-
   login: async function ({ email, password }) {
     const user = await User.findOne({ email: email });
     if (!user) {
-      const error = new Error('User not found');
+      const error = new Error('User not found.');
       error.code = 401;
       throw error;
     }
     const isEqual = await bcrypt.compare(password, user.password);
     if (!isEqual) {
-      const error = new Error('Password is incorrect');
+      const error = new Error('Password is incorrect.');
       error.code = 401;
       throw error;
     }
@@ -62,10 +61,9 @@ module.exports = {
     );
     return { token: token, userId: user._id.toString() };
   },
-
   createPost: async function ({ postInput }, req) {
     if (!req.isAuth) {
-      const error = new Error('Not authenticated');
+      const error = new Error('Not authenticated!');
       error.code = 401;
       throw error;
     }
@@ -110,14 +108,22 @@ module.exports = {
       updatedAt: createdPost.updatedAt.toISOString(),
     };
   },
-  posts: async function (args, req) {
+  posts: async function ({ page }, req) {
     if (!req.isAuth) {
       const error = new Error('Not authenticated!');
       error.code = 401;
       throw error;
     }
+    if (!page) {
+      page = 1;
+    }
+    const perPage = 2;
     const totalPosts = await Post.find().countDocuments();
-    const posts = await Post.find().sort({ createdAt: -1 }).populate('creator');
+    const posts = await Post.find()
+      .sort({ createdAt: -1 })
+      .skip((page - 1) * perPage)
+      .limit(perPage)
+      .populate('creator');
     return {
       posts: posts.map((p) => {
         return {
